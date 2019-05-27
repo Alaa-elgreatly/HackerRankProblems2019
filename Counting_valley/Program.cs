8423b87fab382cd2000c215e2eb797e4ccf15913
Alaa ELGERITLY <alaa.elgeritly@valeo.com>
2019-05-27 13:21:31 +0200

    Added new problem Counting_valley in INTERVIEW PREPARATION KIT- WARM-UP CHALLANGES

---
+++ Counting_valley/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_valley
+{
+    class Program
+    {
+        static int altitude = 0;
+        static int After_Next_Step = 0;
+        static int Valley_count = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(countingValleys(8, "DDUUUUDD").ToString());
+        }
+        static int countingValleys(int n, string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == 'D')
+                    After_Next_Step = altitude - 1;
+                else
+                    After_Next_Step = altitude + 1;
+
+                if (altitude == 0 && After_Next_Step < 0)
+                    Valley_count++;
+                altitude = After_Next_Step;
+
+            }
+            return Valley_count;
+
+        }
+    }
+}
